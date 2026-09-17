6bb662369f26286a28f09e6106119916f0d3148f
bonus3-0 <bonusfocus2561@gmail.com>
2026-09-17 10:04:59 +0700

    Update Examination_2.py

--- Examination_2.py
+++ Examination_2.py
@@ -6,4 +6,12 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-temp = float(input())
+
+temp = float(input("อุณหภูมิร่างกาย: "))
+
+if temp > = ("37.5")
+    print("Fever")
+elif temp < = ("37.5")
+    print("Normal")
+else:
+    print("Normal")
